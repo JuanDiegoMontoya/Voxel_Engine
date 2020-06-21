a969510edf1013cbab4f49fcb3e04c654b337d04
Jake <jake.ryan@digipen.edu>
2020-06-21 01:39:50 -0700

    Added some additional instrumentation.

--- Source/Source/ChunkRenderer.cpp
+++ Source/Source/ChunkRenderer.cpp
@@ -292,6 +292,10 @@ namespace ChunkRenderer
 		if (!dib)
 			return;
 
+#ifdef TRACY_ENABLE
+		TracyGpuZone("Normal Render");
+#endif
+
 		vao->Bind();
 		dib->Bind();
 		//glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
@@ -314,6 +318,10 @@ namespace ChunkRenderer
 	{
 		if (settings.freezeCulling)
 			return;
+
+#ifdef TRACY_ENABLE
+		TracyGpuZone("Occlusion Render");
+#endif
 
 		if (settings.debug_drawOcclusionCulling == false)
 		{
